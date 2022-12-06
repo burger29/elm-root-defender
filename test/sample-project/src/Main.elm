6b255fc21e2d3b31dcf96b05d970e965e5e272fb
burger29 <50500704+burger29@users.noreply.github.com>
2022-12-06 09:24:11 -0500

    test

--- test/sample-project/src/Main.elm
+++ test/sample-project/src/Main.elm
@@ -2,12 +2,13 @@ module Main exposing (main)
 
 import Browser exposing (document)
 import Html
+import Html.Attributes exposing (placeholder)
 
 
 main =
     document
         { init = \() -> ( 0, Cmd.none )
-        , view = \model -> { title = "test", body = [ Html.text "Hello world." ] }
+        , view = \model -> { title = "test", body = [ Html.form [] [ Html.input [ placeholder "type here" ] [], Html.input [ placeholder "type here" ] [], Html.input [ placeholder "type here" ] [], Html.button [] [] ] ] }
         , update = \msg model -> ( 9, Cmd.none )
         , subscriptions = \model -> Sub.none
         }
